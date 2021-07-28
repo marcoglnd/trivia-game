7225ce4b4fefef1b92018e1f3dc44414356e7dcb
Marco Galindo <marco1408@gmail.com>
2021-07-28 18:42:56 -0300

    Updates

---
+++ precache-manifest.e07ede492456bee01b2fe900f0c88375.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b280dbf5056e590dfa879cfdcf8af69c",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "622acd797df0de11e47d",
+    "url": "/trivia-game/static/css/main.3530220a.chunk.css"
+  },
+  {
+    "revision": "c73754fd7a3baa198953",
+    "url": "/trivia-game/static/js/2.3455ec4d.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia-game/static/js/2.3455ec4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "622acd797df0de11e47d",
+    "url": "/trivia-game/static/js/main.da541cdb.chunk.js"
+  },
+  {
+    "revision": "28c450d90070e1cbeaea",
+    "url": "/trivia-game/static/js/runtime-main.34810379.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-game/static/media/trivia.466d153e.png"
+  }
+]);
